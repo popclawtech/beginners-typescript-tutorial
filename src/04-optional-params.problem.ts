4b18c8da5ada0fa45145ddfe92bfe3ce9207218d
Koray Guler <koray.guler@trendyol.com>
2022-10-14 16:10:21 +0300

    add: 04 05 06 07

--- src/04-optional-params.problem.ts
+++ src/04-optional-params.problem.ts
@@ -1,6 +1,7 @@
 import { expect, it } from "vitest";
 
-export const getName = (first: string, last: string) => {
+
+export const getName = (first: string, last?: string) => {
   if (last) {
     return `${first} ${last}`;
   }
